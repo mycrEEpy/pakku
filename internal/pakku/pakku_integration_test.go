25021e509c20ca14cd4aa2edc8fd9a159f4dd163
Tobias <bvrcreepyx@hotmail.de>
2024-12-26 17:31:35 +0100

    add TestBrew

--- internal/pakku/pakku_integration_test.go
+++ internal/pakku/pakku_integration_test.go
@@ -80,3 +80,28 @@ func TestDnf(t *testing.T) {
 	mustSucceed(t, container, []string{"pakku", "update"})
 	mustSucceed(t, container, []string{"pakku", "remove", "dnf", "vim"})
 }
+
+func TestBrew(t *testing.T) {
+	req := testcontainers.ContainerRequest{
+		Image:      "debian:12",
+		Entrypoint: []string{"bash", "-c", "echo ready && sleep 60"},
+		Files:      []testcontainers.ContainerFile{pakkuFile},
+		WaitingFor: wait.ForLog("ready"),
+	}
+
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	defer testcontainers.CleanupContainer(t, container)
+	require.NoError(t, err)
+
+	mustSucceed(t, container, []string{"/bin/bash", "-c", "\"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""})
+
+	mustSucceed(t, container, []string{"pakku", "init"})
+	mustSucceed(t, container, []string{"pakku", "config"})
+	mustSucceed(t, container, []string{"pakku", "add", "brew", "vim"})
+	mustSucceed(t, container, []string{"pakku", "apply"})
+	mustSucceed(t, container, []string{"pakku", "update"})
+	mustSucceed(t, container, []string{"pakku", "remove", "brew", "vim"})
+}
