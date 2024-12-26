05cbf16b7f67ed479fb69f762293c62183fd2685
Tobias <bvrcreepyx@hotmail.de>
2024-12-26 17:24:26 +0100

    add TestDnf

--- internal/pakku/pakku_integration_test.go
+++ internal/pakku/pakku_integration_test.go
@@ -37,7 +37,30 @@ func mustReadAll(reader io.Reader) []byte {
 
 func TestApt(t *testing.T) {
 	req := testcontainers.ContainerRequest{
-		Image:      "public.ecr.aws/docker/library/debian:12",
+		Image:      "debian:12",
+		Entrypoint: []string{"bash", "-c", "echo ready && sleep 60"},
+		Files:      []testcontainers.ContainerFile{pakkuFile},
+		WaitingFor: wait.ForLog("ready"),
+	}
+
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	defer testcontainers.CleanupContainer(t, container)
+	require.NoError(t, err)
+
+	mustSucceed(t, container, []string{"pakku", "init"})
+	mustSucceed(t, container, []string{"pakku", "config"})
+	mustSucceed(t, container, []string{"pakku", "add", "apt", "vim"})
+	mustSucceed(t, container, []string{"pakku", "apply"})
+	mustSucceed(t, container, []string{"pakku", "update"})
+	mustSucceed(t, container, []string{"pakku", "remove", "apt", "vim"})
+}
+
+func TestDnf(t *testing.T) {
+	req := testcontainers.ContainerRequest{
+		Image:      "fedora:41",
 		Entrypoint: []string{"bash", "-c", "echo ready && sleep 60"},
 		Files:      []testcontainers.ContainerFile{pakkuFile},
 		WaitingFor: wait.ForLog("ready"),
