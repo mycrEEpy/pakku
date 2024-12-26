4d64dbc280ac21092a1df7e4f396d9e06e46be10
Tobias <bvrcreepyx@hotmail.de>
2024-12-26 16:49:22 +0100

    fix gitignore

---
+++ internal/pakku/pakku_integration_test.go
@@ -0,0 +1,60 @@
+package pakku
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+var (
+	ctx = context.Background()
+
+	pakkuFile = testcontainers.ContainerFile{
+		HostFilePath:      "pakku",
+		ContainerFilePath: "/usr/local/bin/pakku",
+		FileMode:          0o755,
+	}
+)
+
+func TestApt(t *testing.T) {
+	req := testcontainers.ContainerRequest{
+		Image:      "public.ecr.aws/docker/library/debian:12",
+		Entrypoint: []string{"sleep", "60"},
+		Files:      []testcontainers.ContainerFile{pakkuFile},
+		WaitingFor: wait.ForExit(),
+	}
+
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	defer testcontainers.CleanupContainer(t, container)
+	require.NoError(t, err)
+
+	rc, _, err := container.Exec(ctx, []string{"pakku", "init"})
+	require.NoError(t, err)
+	require.Zero(t, rc)
+
+	rc, _, err = container.Exec(ctx, []string{"pakku", "config"})
+	require.NoError(t, err)
+	require.Zero(t, rc)
+
+	rc, _, err = container.Exec(ctx, []string{"pakku", "add", "apt", "vim"})
+	require.NoError(t, err)
+	require.Zero(t, rc)
+
+	rc, _, err = container.Exec(ctx, []string{"pakku", "apply"})
+	require.NoError(t, err)
+	require.Zero(t, rc)
+
+	rc, _, err = container.Exec(ctx, []string{"pakku", "update"})
+	require.NoError(t, err)
+	require.Zero(t, rc)
+
+	rc, _, err = container.Exec(ctx, []string{"pakku", "remove", "apt", "vim"})
+	require.NoError(t, err)
+	require.Zero(t, rc)
+}
